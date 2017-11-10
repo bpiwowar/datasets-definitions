38934823a5a0175d971fd4a041448ac76e671d07
Benjamin Piwowarski <benjamin@bpiwowar.net>
2017-11-10 18:55:14 +0100

    Re-organized TREC

--- module/handlers/dataset/documents.py
+++ module/handlers/dataset/documents.py
@@ -4,5 +4,10 @@ from datasets.data import Handler, DownloadHandler
 class Collection(Handler):
     """Just a set of document collections"""
     def download(self):
+        for ref in self.content["references"]:
+            ref.download()
         # Verify if the dataset has been downloaded
         return False
+
+    def __repr__(self):
+        return "Collection(%s)" % (", ".join([repr(r) for r in self.content["references"]]))
--- module/handlers/dataset/trec/documents.py
+++ module/handlers/dataset/trec/documents.py
@@ -4,5 +4,13 @@ from datasets.data import Handler, DownloadHandler
 class Sgml(Handler):
     def download(self):
         # Verify if the dataset has been downloaded
-        print(self.dataset)
+        basedir = self.config.datapath.joinpath(*self.dataset.id.split("."))
+        path = self.datapath.with_suffix(".relfiles")
+
+        with open(path, "rt") as fp:
+            for line in fp:
+                path = basedir.joinpath(line.strip())
+                if not path.is_file():
+                    print("%s is not a file" % path)
+
         raise Exception("Not implemented")
